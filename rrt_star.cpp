dfaac56c30354e39b1b7e9b87c0cdfaba4a1e5aa
Jaskaran Singh Sodhi <sodhi.jaskaransingh@gmail.com>
2021-11-27 08:33:23 +0530

    kinematic model works!

--- rrt_star.cpp
+++ rrt_star.cpp
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <map>
 #include <opencv2/opencv.hpp>
+#include <omp.h>
+#include <cmath>
 
 struct Node
 {
@@ -25,14 +27,15 @@ class Map
 {
 public:
     int found = 0;
-    int height, width;
+    int height, width, randomize_obstacles;
+    int num_random_obstacles, rand_obst_max_height;
     double step_size, search_radius;
     struct Node* start = new Node();
     struct Node* goal = new Node();
     std::vector<struct Node*> nodes;
     cv::Mat world;
 
-    Map(int h, int w, struct Node* s, struct Node* g, double s_s, int s_r)
+    Map(int h, int w, struct Node* s, struct Node* g, double s_s, int s_r, int r_o, int n_r_o, int r_o_m_h)
     {
         height = h;
         width = w;
@@ -46,6 +49,10 @@ public:
         
         step_size = s_s;
         search_radius = s_r;
+
+        randomize_obstacles = r_o;
+        num_random_obstacles = n_r_o;
+        rand_obst_max_height = r_o_m_h;
         
         world = cv::Mat(height, width, CV_8UC3, cv::Scalar(255,255,255));
         cv::circle(world, cv::Point(start->x,start->y), 5, cv::Scalar(0,255,0), cv::FILLED, cv::LINE_8);
@@ -104,63 +111,28 @@ public:
     }
     bool is_obstacle(struct Node* node)
     {
-        if(world.at<cv::Vec3b>(node->x,node->y)[0] == 0)
+        if( world.at<cv::Vec3b>(node->y,node->x)[0] != 255 &&
+            world.at<cv::Vec3b>(node->y,node->x)[1] != 255 &&
+            world.at<cv::Vec3b>(node->y,node->x)[2] != 255)
             return true;
         return false;
     }
     bool obstacle_in_path(struct Node* node_1, struct Node* node_2)
     {
-        if(node_1->x < node_2->x)
+        int resolution = int(50);
+
+        // #pragma omp parallel for
+        for(int i = 0; i <= resolution; i++)
         {
-            if(node_1->y < node_2->y)
-            {
-                for(int i = node_1->x + 1; i <= node_2->x; i++)
-                {
-                    for(int j = node_1->y + 1; j <= node_2->y; j++)
-                    {
-                        if(world.at<cv::Vec3b>(i,j)[0] == 0)
-                            return false;
-                    }
-                }
-            }
-            else
-            {
-                for(int i = node_1->x + 1; i <= node_2->x; i++)
-                {
-                    for(int j = node_2->y + 1; j <= node_1->y; j++)
-                    {
-                        if(world.at<cv::Vec3b>(i,j)[0] == 0)
-                            return false;
-                    }
-                }
-            }
+            int x_check = round((i * node_1->x + (resolution - i) * node_2->x)/resolution);
+            int y_check = round((i * node_1->y + (resolution - i) * node_2->y)/resolution);
+
+            if( world.at<cv::Vec3b>(y_check,x_check)[0] != 255 &&
+                world.at<cv::Vec3b>(y_check,x_check)[1] != 255 &&
+                world.at<cv::Vec3b>(y_check,x_check)[2] != 255)
+                return true;
         }
-        else
-        {
-            if(node_1->y < node_2->y)
-            {
-                for(int i = node_2->x + 1; i <= node_1->x; i++)
-                {
-                    for(int j = node_1->y + 1; j <= node_2->y; j++)
-                    {
-                        if(world.at<cv::Vec3b>(i,j)[0] == 0)
-                            return false;
-                    }
-                }
-            }
-            else
-            {
-                for(int i = node_2->x + 1; i <= node_1->x; i++)
-                {
-                    for(int j = node_2->y + 1; j <= node_1->y; j++)
-                    {
-                        if(world.at<cv::Vec3b>(i,j)[0] == 0)
-                            return false;
-                    }
-                }
-            }
-        }
-        return true;
+        return false;
     }
     void add_obstacle(int x, int y, int height, int width)
     {
@@ -171,6 +143,21 @@ public:
                 world.at<cv::Vec3b>(i,j)[1] = 0;
                 world.at<cv::Vec3b>(i,j)[2] = 0;
             }
+        
+        // clearance
+        for(int i = -10; i < 10; i++)
+        {
+            for(int j = -10; j < 10; j++)
+            {
+                world.at<cv::Vec3b>(start->x + i,start->y + j)[0] = 255;
+                world.at<cv::Vec3b>(start->x + i,start->y + j)[1] = 255;
+                world.at<cv::Vec3b>(start->x + i,start->y + j)[2] = 255;
+
+                world.at<cv::Vec3b>(goal->x + i,goal->y + j)[0] = 255;
+                world.at<cv::Vec3b>(goal->x + i,goal->y + j)[1] = 255;
+                world.at<cv::Vec3b>(goal->x + i,goal->y + j)[2] = 255;
+            }
+        }
     }
     void display_map(bool found = false)
     {
@@ -182,9 +169,9 @@ public:
             cv::Point point_1 = cv::Point(nodes[i]->x,nodes[i]->y);
             cv::Point point_2 = cv::Point(nodes[i]->parent->x,nodes[i]->parent->y);
 
-            cv::line(world, point_1, point_2, cv::Scalar(0,0,0), 1, cv::LINE_8); 
+            cv::line(world, point_1, point_2, cv::Scalar(0,255,0), 1, cv::LINE_8); 
         }
-        cv::imshow("img",world);
+        cv::imshow("Output Window",world);
     }
     void display_final_path()
     {
@@ -202,7 +189,7 @@ public:
         cv::Point point_2 = cv::Point(curr_node->parent->x,curr_node->parent->y);
         cv::line(world, point_1, point_2, cv::Scalar(0,0,255), 2, cv::LINE_8); 
 
-        cv::imshow("img",world);
+        cv::imshow("Output Window",world);
     }
     void display_nodes()
     {
@@ -214,16 +201,42 @@ public:
         std::vector<struct Node*> neighbours;
         for(int i = 0; i < nodes.size(); i++)
         {
+            if(nodes[i] == node)
+                continue;
+
+            if(nodes[i] == node->parent)
+                continue;
+            
+            if(obstacle_in_path(nodes[i],node))
+                continue;
+
             if(euclidean_distance(node,nodes[i]) < search_radius)
                 neighbours.push_back(nodes[i]);
         }
         return neighbours;
     }
-    void rewire(struct Node* node, std::vector<struct Node*> neighbours)
+    void set_obstacles(int random_obstacles[][4])
+    {
+        if(randomize_obstacles)
+            set_random_obstacles(random_obstacles);
+        else
+        {
+            // add_obstacle(50,50,50,300);
+            // add_obstacle(300,50,200,50);
+            // add_obstacle(100,200,50,250);
+            add_obstacle(100,100,200,200);
+        }
+    }
+    void rewire(struct Node* node, std::vector<struct Node*> neighbours, double rho_min, int random_obstacles[][4])
     {
         world = cv::Mat(height, width, CV_8UC3, cv::Scalar(255,255,255));
+        set_obstacles(random_obstacles);
         for(int i = 0; i < neighbours.size(); i++)
         {
+            if(obstacle_in_path(node,neighbours[i]))
+                continue;
+            if(!check_kino(node,neighbours[i],rho_min))
+                continue;
             if(neighbours[i] == node->parent)
                 continue;
             
@@ -233,23 +246,144 @@ public:
                 neighbours[i]->cost = node->cost + euclidean_distance(node,neighbours[i]);
             }
         }
+
+        world = cv::Mat(height, width, CV_8UC3, cv::Scalar(255,255,255));
+        set_obstacles(random_obstacles);
+    }
+    bool check_kino(struct Node* node_1, struct Node* node_2, double rho_min)
+    {
+        int x1 = node_1->x;
+        int y1 = node_1->y;
+        int x2 = node_2->x;
+        int y2 = node_2->y;
+        double theta = node_1->orientation;
+
+        double num = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+        double den = (2 * (x2 - x1) * sin(theta) - 2 * (y2 - y1) * cos(theta));
+
+        double abs_den = abs(den);
+
+        if(abs_den < 0.01)
+            return true;
+
+        double rho = 0.0, xc = 0.0, yc = 0.0;
+        rho = num/abs_den;
+
+        if(den < 0)
+        {
+            xc = x1 - rho * sin(theta);
+            yc = y1 + rho * cos(theta);
+        }
+
+        else
+        {
+            xc = x1 + rho * sin(theta);
+            yc = y1 - rho * cos(theta);
+        }
+
+        if(rho > rho_min)
+            return true;
+            
+        return false;
+    }
+    double set_goal_cost()
+    {
+        struct Node* curr_node = goal;
+        double cost = 0.0;
+        while(curr_node->parent != NULL)
+        {
+            cost += euclidean_distance(curr_node,curr_node->parent);
+            curr_node = curr_node->parent;
+        }
+        return cost;
+    }
+    void set_node_costs(double initial_orientation)
+    {
+        nodes[0]->cost = 0.0;
+        nodes[0]->orientation = initial_orientation;
+
+        for(int i = 1; i < nodes.size(); i++)
+        {
+            nodes[i]->cost = nodes[i]->parent->cost + euclidean_distance(nodes[i],nodes[i]->parent);
+
+            int x1 = nodes[i]->parent->x;
+            int y1 = nodes[i]->parent->y;
+            int x2 = nodes[i]->x;
+            int y2 = nodes[i]->y;
+            double theta = nodes[i]->parent->orientation;
+
+            double num = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+            double den = (2 * (x2 - x1) * sin(theta) - 2 * (y2 - y1) * cos(theta));
+
+            double abs_den = abs(den);
+
+            double rho = 0.0, xc = 0.0, yc = 0.0;
+            rho = num/abs_den;
+
+            if(den < 0)
+            {
+                xc = x1 - rho * sin(theta);
+                yc = y1 + rho * cos(theta);
+
+                nodes[i]->orientation = asin((x1 - x2)/rho - sin(theta));
+            }
+
+            else
+            {
+                xc = x1 + rho * sin(theta);
+                yc = y1 - rho * cos(theta);
+                
+                nodes[i]->orientation = asin((x1 - x2)/rho + sin(theta));
+            }
+        }
+
+    }
+
+    void set_random_obstacles(int random_obstacles[][4])
+    {
+        for(int i = 0; random_obstacles[i][0] != '\0'; i++)
+        {
+            add_obstacle(random_obstacles[i][0], random_obstacles[i][1], random_obstacles[i][2], random_obstacles[i][3]);
+        }
     }
 };
 
 int main()
 {
     int height = 400;
-    int width = 600;
+    int width = 400;
+    int randomize_obstacles = 1;
+    int num_random_obstacles = 100;
+    int rand_obst_min_height = 8;
+    int rand_obst_max_height = 12;
+
+    int random_obstacles[num_random_obstacles][4];
+
+    for(int i = 0; i < num_random_obstacles; i++)
+    {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_real_distribution<double> dist_width(0.0, float(width - rand_obst_max_height - 1)); 
+        std::uniform_real_distribution<double> dist_height(0.0, float(height - rand_obst_max_height - 1));
+        std::uniform_real_distribution<double> dist_dim_1(8.0, float(rand_obst_max_height));
+        std::uniform_real_distribution<double> dist_dim_2(8.0, float(rand_obst_max_height));
+
+        random_obstacles[i][0] = dist_width(mt);
+        random_obstacles[i][1] = dist_height(mt);
+        random_obstacles[i][2] = dist_dim_1(mt);
+        random_obstacles[i][3] = dist_dim_2(mt);
+    }
 
     struct Node* start = new Node(40,40);
-    start->cost = 0;
-    struct Node* goal = new Node(350,350);
+    start->cost = 0.0;
+    struct Node* goal = new Node(320,320);
 
-    double step_size =15.0, search_radius = 19.0 ;
+    double step_size = 30.0, search_radius = 40.0, rho_min = 100, initial_orientation = 3 * 3.1415 / 2;
     int ITERATIONS = 200000;
 
-    Map map(height,width,start,goal,step_size,search_radius);
-    map.add_obstacle(100,100,200,200);
+    Map map(height,width,start,goal,step_size,search_radius,randomize_obstacles,num_random_obstacles, rand_obst_max_height);
+
+    map.set_obstacles(random_obstacles);
 
     struct Node* rand_node = new Node(start->x, start->y);
     struct Node* steered_node_global = new Node(start->x, start->y);
@@ -265,10 +399,6 @@ int main()
         nearest_node = map.find_nearest(rand_node);
 
         struct Node* steered_node = new Node(map.steer(rand_node,nearest_node)->x, map.steer(rand_node,nearest_node)->y);
-        steered_node->parent = nearest_node;
-        steered_node->cost = nearest_node->cost + map.euclidean_distance(nearest_node,steered_node);
-        
-        steered_node_global = steered_node;
     
         if(map.is_obstacle(steered_node))
             continue;
@@ -276,24 +406,31 @@ int main()
         if(map.obstacle_in_path(nearest_node,steered_node))
             continue;
         
+        if(!map.check_kino(nearest_node,steered_node, rho_min))
+            continue;
+
+        steered_node->parent = nearest_node;
+        steered_node->cost = nearest_node->cost + map.euclidean_distance(nearest_node,steered_node);
+        
+        steered_node_global = steered_node;
+        
         map.nodes.push_back(steered_node);
 
         std::vector<struct Node*> neighbours = map.find_neighbours(steered_node);
 
-        map.rewire(steered_node,neighbours);
+        map.rewire(steered_node,neighbours,rho_min, random_obstacles);
 
-        map.world = cv::Mat(height, width, CV_8UC3, cv::Scalar(255,255,255));
-        map.add_obstacle(100,100,200,200);
         cv::circle(map.world, cv::Point(start->x,start->y), 5, cv::Scalar(0,255,0), cv::FILLED, cv::LINE_8);
         cv::circle(map.world, cv::Point(goal->x,goal->y), 5, cv::Scalar(0,0,255), cv::FILLED, cv::LINE_8);
 
         map.display_map();
 
         cv::waitKey(2);
+        map.set_node_costs(initial_orientation);
     }
 
     map.goal->parent = steered_node_global;
-    map.goal->cost = steered_node_global->cost + map.euclidean_distance(steered_node_global,map.goal);
+    map.goal->cost = map.set_goal_cost();
     map.nodes.push_back(map.goal);
 
     cv::Point point_1 = cv::Point(steered_node_global->x,steered_node_global->y);
@@ -311,9 +448,10 @@ int main()
 
     // cv::waitKey(0);
 
+    // #pragma omp parallel for
     for(int iter = 0; iter < ITERATIONS; iter++)
     {
-        std::cout << "Iter [" << iter << "/" << ITERATIONS << "]" << std::endl;
+        std::cout << "Iter [" << iter << "/" << ITERATIONS << "] : " << map.goal->cost << std::endl;
         rand_node = map.sample();
 
         if(rand_node->x == start->x && rand_node->y == start->y)
@@ -322,9 +460,6 @@ int main()
         nearest_node = map.find_nearest(rand_node);
 
         struct Node* steered_node = new Node(map.steer(rand_node,nearest_node)->x, map.steer(rand_node,nearest_node)->y);
-        steered_node->parent = nearest_node;
-        
-        steered_node_global = steered_node;
     
         if(map.is_obstacle(steered_node))
             continue;
@@ -332,15 +467,22 @@ int main()
         if(map.obstacle_in_path(nearest_node,steered_node))
             continue;
         
+        if(!map.check_kino(nearest_node,steered_node, rho_min))
+        {
+            --iter;
+            continue;
+        }
+
+        steered_node->parent = nearest_node;
         steered_node->cost = nearest_node->cost + map.euclidean_distance(nearest_node,steered_node);
+        steered_node_global = steered_node;
+        
         map.nodes.push_back(steered_node);
 
         std::vector<struct Node*> neighbours = map.find_neighbours(steered_node);
 
-        map.rewire(steered_node,neighbours);
+        map.rewire(steered_node,neighbours,rho_min, random_obstacles);
 
-        map.world = cv::Mat(height, width, CV_8UC3, cv::Scalar(255,255,255));
-        map.add_obstacle(100,100,200,200);
         cv::circle(map.world, cv::Point(start->x,start->y), 5, cv::Scalar(0,255,0), cv::FILLED, cv::LINE_8);
         cv::circle(map.world, cv::Point(goal->x,goal->y), 5, cv::Scalar(0,0,255), cv::FILLED, cv::LINE_8);
 
@@ -348,6 +490,9 @@ int main()
         map.display_final_path();
 
         cv::waitKey(2);
+
+        map.goal->cost = map.set_goal_cost();
+        map.set_node_costs(initial_orientation);
     }
     cv::waitKey(0);
     cv::destroyAllWindows();
